b2f8c7aa41799386e2ec080e02294839456e2e79
Ralph Woiwode <rawoiwode@gmail.com>
2024-01-15 14:30:13 -0600

    Creating base for CreateProjectForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import Sidebar from "./components/Sidebar";
 import image from "./assets/no-projects.png";
+import CreateProjectForm from "./components/CreateProjectForm";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <Sidebar />
       <div className="w-2/3 mt-24 text-center">
-        <img
+        {/* <img
           className="w-16 mx-auto object-contain"
           src={image}
           alt="No projects images"
@@ -19,7 +20,8 @@ function App() {
         </p>
         <button className="bg-stone-800 text-stone-400 px-4 py-2 rounded-md">
           Create new project
-        </button>
+        </button> */}
+        <CreateProjectForm />
       </div>
     </main>
   );
