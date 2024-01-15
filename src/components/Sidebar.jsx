6c6f0fdd081d58d780b902e67cc198ae60b22e59
Ralph Woiwode <rawoiwode@gmail.com>
2024-01-15 13:57:58 -0600

    Adding base for Sidebar component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <aside className="w-1/3 px-8 py-16 bg-slate-950 text-stone-50 md:w-72 rounded-r-xl">
+      <h2 className="mb-8  font-bold uppercase md:text-xl text-stone-200">
+        Your Projects
+      </h2>
+    </aside>
+  );
+};
+
+export default Sidebar;
